28a5b759da172c540c36822c24b178fbfada082c
wisher-18 <johnnysonn10@gmail.com>
2024-06-01 12:30:35 +0530

    Build multiple components

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,28 @@
+<x-layout>
+<x-breadcrumbs class="mb-4"
+    :links="['jobs' => route('jobs.index')]"/>
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" value="" placeholder="Search for any text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-2">
+                <x-text-input name="min_salary" value="" placeholder="From" />
+                <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
+        </div>
+    </x-card>
+    @foreach ($jobs as $job)
+    <x-job-card class="mb-4" :$job>
+        <div>
+            <x-link-button :href="route('jobs.show', $job)">
+                Show
+            </x-link-button>
+        </div>
+    </x-job-card>
+    @endforeach
+</x-layout>
---
+++ resources/views/job/show.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-breadcrumbs class="mb-4"
+    :links="['jobs' => route('jobs.index'), $job->title => '#']"/>
+    <x-job-card :$job>
+    <p class="text-sm text-slate-500 mb-4">
+        {!! nl2br(e($job->description)) !!}
+    </p>
+    </x-job-card>
+</x-layout>
